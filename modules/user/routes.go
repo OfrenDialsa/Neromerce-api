2bd788e225290e831c24c5f555549a7f03cdf424
OfrenDialsa <ofrendialsa.dev@gmail.com>
2025-12-07 20:26:23 +0700

    initial commit

---
+++ modules/user/routes.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ofrendialsa/neromerce/middlewares"
+	"github.com/ofrendialsa/neromerce/modules/auth/service"
+	"github.com/ofrendialsa/neromerce/modules/user/controller"
+	"github.com/ofrendialsa/neromerce/pkg/constants"
+	"github.com/samber/do"
+)
+
+func RegisterRoutes(server *gin.Engine, injector *do.Injector) {
+	userController := do.MustInvoke[controller.UserController](injector)
+	jwtService := do.MustInvokeNamed[service.JWTService](injector, constants.JWTService)
+
+	userRoutes := server.Group("/api/user")
+	{
+		userRoutes.GET("", userController.GetAllUser)
+		userRoutes.GET("/me", middlewares.Authenticate(jwtService), userController.Me)
+		userRoutes.PUT("/:id", middlewares.Authenticate(jwtService), userController.Update)
+		userRoutes.DELETE("/:id", middlewares.Authenticate(jwtService), userController.Delete)
+	}
+}
---
+++ providers/core.go
@@ -0,0 +1,49 @@
+package providers
+
+import (
+	"github.com/ofrendialsa/neromerce/config"
+	authController "github.com/ofrendialsa/neromerce/modules/auth/controller"
+	authRepo "github.com/ofrendialsa/neromerce/modules/auth/repository"
+	authService "github.com/ofrendialsa/neromerce/modules/auth/service"
+	userController "github.com/ofrendialsa/neromerce/modules/user/controller"
+	"github.com/ofrendialsa/neromerce/modules/user/repository"
+	userService "github.com/ofrendialsa/neromerce/modules/user/service"
+	"github.com/ofrendialsa/neromerce/pkg/constants"
+	"github.com/samber/do"
+	"gorm.io/gorm"
+)
+
+func InitDatabase(injector *do.Injector) {
+	do.ProvideNamed(injector, constants.DB, func(i *do.Injector) (*gorm.DB, error) {
+		return config.SetUpDatabaseConnection(), nil
+	})
+}
+
+func RegisterDependencies(injector *do.Injector) {
+	InitDatabase(injector)
+
+	do.ProvideNamed(injector, constants.JWTService, func(i *do.Injector) (authService.JWTService, error) {
+		return authService.NewJWTService(), nil
+	})
+
+	db := do.MustInvokeNamed[*gorm.DB](injector, constants.DB)
+	jwtService := do.MustInvokeNamed[authService.JWTService](injector, constants.JWTService)
+
+	userRepository := repository.NewUserRepository(db)
+	refreshTokenRepository := authRepo.NewRefreshTokenRepository(db)
+
+	userService := userService.NewUserService(userRepository, db)
+	authService := authService.NewAuthService(userRepository, refreshTokenRepository, jwtService, db)
+
+	do.Provide(
+		injector, func(i *do.Injector) (userController.UserController, error) {
+			return userController.NewUserController(i, userService), nil
+		},
+	)
+
+	do.Provide(
+		injector, func(i *do.Injector) (authController.AuthController, error) {
+			return authController.NewAuthController(i, authService), nil
+		},
+	)
+}
