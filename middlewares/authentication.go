fcb6c1b5b1b93ea91a5398c44c2323e48b150ba3
OfrenDialsa <ofrendialsa.dev@gmail.com>
2026-01-01 22:59:19 +0700

    fix: unable to logout

--- middlewares/authentication.go
+++ middlewares/authentication.go
@@ -13,9 +13,16 @@ import (
 func Authenticate(jwtService service.JWTService) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		authHeader := ctx.GetHeader("Authorization")
-		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
-			res := utils.BuildResponseFailed(dto.MESSAGE_FAILED_PROSES_REQUEST, dto.MESSAGE_FAILED_TOKEN_NOT_FOUND, nil)
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, res)
+
+		if authHeader == "" {
+			response := utils.BuildResponseFailed(dto.MESSAGE_FAILED_PROSES_REQUEST, dto.MESSAGE_FAILED_TOKEN_NOT_FOUND, nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			response := utils.BuildResponseFailed(dto.MESSAGE_FAILED_PROSES_REQUEST, dto.MESSAGE_FAILED_TOKEN_NOT_VALID, nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
 			return
 		}
 
@@ -24,27 +31,28 @@ func Authenticate(jwtService service.JWTService) gin.HandlerFunc {
 
 		token, err := jwtService.ValidateToken(tokenStr)
 		if err != nil || !token.Valid {
-			res := utils.BuildResponseFailed(dto.MESSAGE_FAILED_PROSES_REQUEST, dto.MESSAGE_FAILED_TOKEN_NOT_VALID, nil)
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, res)
+			response := utils.BuildResponseFailed(dto.MESSAGE_FAILED_PROSES_REQUEST, dto.MESSAGE_FAILED_TOKEN_NOT_VALID, nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
 			return
 		}
 
 		userId, err := jwtService.GetUserIDByToken(tokenStr)
 		if err != nil {
-			res := utils.BuildResponseFailed(dto.MESSAGE_FAILED_PROSES_REQUEST, err.Error(), nil)
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, res)
+			response := utils.BuildResponseFailed(dto.MESSAGE_FAILED_PROSES_REQUEST, err.Error(), nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
 			return
 		}
 
-		role, err := jwtService.GetUserRoleByToken(tokenStr)
+		userRole, err := jwtService.GetUserRoleByToken(tokenStr)
 		if err != nil {
-			res := utils.BuildResponseFailed(dto.MESSAGE_FAILED_PROSES_REQUEST, "Gagal mendapatkan role", nil)
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, res)
+			response := utils.BuildResponseFailed(dto.MESSAGE_FAILED_PROSES_REQUEST, "Gagal mendapatkan role", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
 			return
 		}
 
+		ctx.Set("token", tokenStr)
 		ctx.Set("user_id", userId)
-		ctx.Set("user_role", role)
+		ctx.Set("user_role", userRole)
 
 		ctx.Next()
 	}
